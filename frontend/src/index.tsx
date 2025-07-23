d77e7545c0f13478a926197cf2f16aaa017ee8e9
MD RAZA KHAN <kmdraza47@gmail.com>
2025-07-23 15:05:28 +0530

    json files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
-import './App.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root');
-
-if (container) {
-  const root = ReactDOM.createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  console.error("Target container is not found in the DOM.");
-}
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
